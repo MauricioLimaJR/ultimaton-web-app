6f3c2626583e4fb8778e54ffa67c934353109e10
Maurício Lima <mauricio.lima@catho.com>
2021-02-24 17:30:03 -0300

    App background color
    - added colors and background color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { BrowserRouter } from 'react-router-dom'
+import styled from 'styled-components'
+// Others
+import * as colors from './constants/colors'
+
+const Canvas = styled.div`
+  background-color: ${colors.godGray};
+  color: ${colors.white};
+  height: 100vh;
+  font-family: ${'!important Roboto, Regular, Helvetica'};
+  overflow: hidden;
+  text-align: center;
+`
 
 function App() {
   return (
-    <div className="App">
+    <Canvas className="App">
       <BrowserRouter>{'Ultimaton App'}</BrowserRouter>
-    </div>
+    </Canvas>
   )
 }
 
---
+++ src/constants/colors.js
@@ -0,0 +1,21 @@
+/**
+ * @name Colors
+ * @description
+ * Application colors assigned to names.
+ * @see http://chir.ag/projects/name-that-color
+ */
+
+// Marvel color palete
+
+export const brownDerby = '#4D1518'
+export const oldBrick = '#951E22'
+export const cardinal = '#C72523'
+export const pumice = '#B9BEBA'
+export const steelBlue = '#4D8AB5'
+export const mirage = '#1E2639'
+
+// Default color palete
+
+export const godGray = '#151515'
+export const tundora = '#444444'
+export const white = '#FFFFFF'
